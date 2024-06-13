e580f362f65d9da348c54e21cad92eafdd8727c2
sauergru <sauergru@gmail.com>
2024-06-13 21:30:01 +0200

    lesson 12 01

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,36 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { actionDeposit, actionWithdraw } from '../store/balanceSlice';
+import { useState } from 'react';
+
+const HomePage = () => {
+  const balanceValue = useSelector(state => state.balance.value);
+  const dispatch = useDispatch();
+  const [value, setValue] = useState('');
+
+  const handleDeposit = () => {
+    dispatch(actionDeposit(value));
+  };
+
+  const handleWithdraw = () => {
+    dispatch(actionWithdraw(value));
+  };
+
+  return (
+    <div>
+      Home Page
+      <hr />
+      Balance: {balanceValue}
+      <hr />
+      <input
+        type="number"
+        value={value}
+        onChange={({ target: { value } }) => setValue(Number(value))}
+      />
+      <hr />
+      <button onClick={handleDeposit}>Deposit</button>
+      <button onClick={handleWithdraw}>Withdraw</button>
+    </div>
+  );
+};
+
+export default HomePage;
